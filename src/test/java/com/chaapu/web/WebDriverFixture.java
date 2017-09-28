3fa74b1db88af94f006e1fbb856a4c085b587a30
Prateek Sharma <message2prateek@gmail.com>
2017-09-28 23:04:08 +1300

    Added Web Tests

---
+++ src/test/java/com/chaapu/web/WebDriverFixture.java
@@ -0,0 +1,62 @@
+package com.chaapu.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+
+
+public class WebDriverFixture {
+    private static final Log LOG = LogFactory.getFactory().getInstance(WebDriverFixture.class);
+    private static WebDriver driver;
+
+    private static long startTimeInSeconds;
+
+    public WebDriverFixture() {
+        LOG.info("Running tests (" + this.getClass().getSimpleName() + ")");
+    }
+
+    private static void signalStartExecution() {
+        startTimeInSeconds = System.currentTimeMillis() / 1000;
+    }
+
+    private static String getTimeSoFarInSeconds() {
+        long timeNow = System.currentTimeMillis() / 1000;
+
+        String secondsTaken = String.valueOf(timeNow - startTimeInSeconds);
+        LOG.info("Time taken (secs) for specification execution so far: " + secondsTaken);
+        return secondsTaken;
+    }
+
+    protected static WebDriver getWebDriver() {
+        return driver;
+    }
+
+    @BeforeClass
+    public static void startTimer() {
+        signalStartExecution();
+    }
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Prateek\\Softwares\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @AfterClass
+    public static void stopTimer() {
+        getTimeSoFarInSeconds();
+    }
+}
---
+++ src/test/java/com/chaapu/web/WebTest.java
@@ -0,0 +1,26 @@
+package com.chaapu.web;
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+@RunWith(ConcordionRunner.class)
+public class WebTest extends WebDriverFixture {
+    public void navigateToPage() {
+        getWebDriver().get(System.getProperty("user.dir") + "/src/main/java/com/chaapu/web/HelloWorld.html");
+    }
+
+    public boolean isHeadingDisplayed() {
+        WebElement heading = new WebDriverWait(getWebDriver(), 10).until(ExpectedConditions.visibilityOfElementLocated(By.id("heading")));
+        return heading
+                .getText()
+                .equals("This below is a list of important stuff");
+    }
+
+    public boolean areItemsDisplayed() {
+        return getWebDriver().findElements(By.tagName("li")).size() == 3;
+    }
+}
