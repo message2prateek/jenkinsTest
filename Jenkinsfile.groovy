9641b3294647e5f3450e754786b1bc5d83ec7c37
Prateek Sharma <message2prateek@gmail.com>
2017-10-07 16:25:35 +1300

    Fixed Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,20 +2,26 @@
 
 pipeline {
     agent any
-stages {
-   stage('Pull Code') {
-       git 'https://github.com/message2prateek/jenkinsTest.git'
-   }
+    stages {
+        stage('Pull Code') {
+            steps {
+                git 'https://github.com/message2prateek/jenkinsTest.git'
+            }
+        }
 
-   stage('Run unit test'){
-       sh './gradlew runUnitTests'
-       publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/reports/tests/runUnitTests', reportFiles: 'index.html', reportName: 'Unit Tests Report', reportTitles: ''])
-   }
-   wrap([$class: 'Xvfb', additionalOptions: '', assignedLabels: '', displayName: 99, displayNameOffset: 0, installationName: 'default Xvfb', timeout: 10]) {
-         stage('Run integration test'){
-           sh './gradlew runIntegrationTests'
-           publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/reports/tests/runIntegrationTests', reportFiles: 'index.html', reportName: 'Integration Tests Report', reportTitles: ''])
-       }
-   }
-}
+        stage('Run unit test') {
+            steps {
+                sh './gradlew runUnitTests'
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/reports/tests/runUnitTests', reportFiles: 'index.html', reportName: 'Unit Tests Report', reportTitles: ''])
+            }
+        }
+        wrap([$class: 'Xvfb', additionalOptions: '', assignedLabels: '', displayName: 99, displayNameOffset: 0, installationName: 'default Xvfb', timeout: 10]) {
+            stage('Run integration test') {
+                steps {
+                    sh './gradlew runIntegrationTests'
+                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/reports/tests/runIntegrationTests', reportFiles: 'index.html', reportName: 'Integration Tests Report', reportTitles: ''])
+                }
+            }
+        }
+    }
 }
