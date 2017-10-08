18b021583d66127addcadcae3156d8eea221e29e
Prateek Sharma <message2prateek@gmail.com>
2017-10-08 14:10:54 +1300

    Modified Jenkinsfile
    Added options section for clearing builds
    Added Junit xml reports
    Renamed gradle test from runIntegrationTests to runAcceptanceTests

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,8 +2,14 @@
 
 pipeline {
     agent any
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '5'))
+        timeout(time: 1, unit: 'HOURS')
+    }
+
     stages {
-        stage('Pull Code') {
+        stage('Checkout Code') {
             steps {
                 git 'https://github.com/message2prateek/jenkinsTest.git'
             }
@@ -12,14 +18,24 @@ pipeline {
         stage('Run unit test') {
             steps {
                 sh './gradlew runUnitTests'
-                publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/reports/tests/runUnitTests', reportFiles: 'index.html', reportName: 'Unit Tests Report', reportTitles: ''])
+            }
+            post {
+                always {
+                    junit 'build/test-results/runUnitTests/*.xml'
+                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/reports/tests/runUnitTests', reportFiles: 'index.html', reportName: 'Unit Tests Report', reportTitles: ''])
+                }
             }
         }
-        stage('Run integration test') {
+        stage('Run Acceptance test') {
             steps {
                 wrap([$class: 'Xvfb', additionalOptions: '', assignedLabels: '', displayName: 99, displayNameOffset: 0, installationName: 'default Xvfb', timeout: 10]) {
-                    sh './gradlew runIntegrationTests'
-                    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/reports/tests/runIntegrationTests', reportFiles: 'index.html', reportName: 'Integration Tests Report', reportTitles: ''])
+                    sh './gradlew runAcceptanceTests'
+                }
+                post {
+                    always {
+                        junit 'build/test-results/runAcceptanceTests/*.xml'
+                        publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'build/reports/tests/runIntegrationTests', reportFiles: 'index.html', reportName: 'Acceptance Tests Report', reportTitles: ''])
+                    }
                 }
             }
         }
